1e4f1324c4681fd770934505daa688683c940bbf
saljaoui <soufian2022ff@gmail.com>
2025-05-11 22:52:48 +0100

    update

--- partial_sums/src/lib.rs
+++ partial_sums/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn parts_sums(arr: &[u64]) -> Vec<u64> {
-
+    let mut res = vec![0];
+    let mut sum = 0;
+    for v in arr {
+        sum += v;
+        res.push(sum);
+    }
+    res.into_iter().rev().collect()
 }
