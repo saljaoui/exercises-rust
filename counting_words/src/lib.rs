2dbf80e75faf161c014b1fb4372fe1043db34303
saljaoui <soufian2022ff@gmail.com>
2025-05-11 19:31:26 +0100

    update

--- counting_words/src/lib.rs
+++ counting_words/src/lib.rs
@@ -1,14 +1,16 @@
 use std::collections::HashMap;
 pub fn counting_words(words: &str) -> HashMap<String, u32> {
     let mut hashmap = HashMap::new();
-    let vec: Vec<&str> =  words.split(" ").collect();
+    let vec: Vec<&str> =  words.split_whitespace().collect();
     for v in vec {
     let mut res: String = String::new(); 
     for i in v.chars() {
-        if i != '!' &&  i != ',' && i != ';' && i != '.' && i != '―' && i != '/' && i != '“' && i != '\n' && i != '”' {
+        if i.is_alphanumeric() || i == '\'' {
             res.push_str(&i.to_ascii_lowercase().to_string())
         }
     }
+    let res = res.trim_matches('\'').to_string();
+    
     if !res.is_empty() {
         let count = hashmap.entry(res).or_insert(0);
         *count += 1
