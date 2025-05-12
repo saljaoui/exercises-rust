521d8ef3c9e27458c6ce298dedb858759e8d294b
saljaoui <soufian2022ff@gmail.com>
2025-05-12 10:35:06 +0100

    update

---
+++ insertion_sort/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn insertion_sort(slice: &mut [i32], steps: usize) {
+    for i in 0..=steps {
+        for j in 0..i+1 {
+            if slice[i] < slice[j] {
+                slice.swap(j, i);
+            }
+        }
+    }
+
+}
