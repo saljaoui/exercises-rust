b795b8ee9e3b19a12271b864e7079b4ff433c3f5
saljaoui <soufian2022ff@gmail.com>
2025-05-18 23:26:12 +0100

    update

--- lucas_number/src/lib.rs
+++ lucas_number/src/lib.rs
@@ -1,13 +1,11 @@
 pub fn lucas_number(n: u32) -> u32 {
-    let mut r: u32 = 2;
-    let mut a = 2;
-    let mut b = 1;
-
-    for _i in 1..n {
-        r = a + b;
-        b = r;
+    let mut res: u32 = 0;
+    let mut a: u32 = 2;
+    let mut b: u32 = 1;
+    for i in 2..=n {
+        res = a + b;
         a = b;
+        b = res;
     }
-    
-    return r
+    res
 }
