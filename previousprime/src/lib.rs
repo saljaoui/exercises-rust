2a5990019ca04519b782628c09a4a5733cc6c45a
saljaoui <soufian2022ff#gmail.com>
2025-05-18 18:08:50 +0100

    update

---
+++ previousprime/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn prev_prime(nbr: u64) -> u64  {
+    if nbr <= 2 {
+        return 0
+    }
+    for n in (1..nbr).rev() {
+        if check(n) {
+            return n;
+        }
+    }
+    0
+
+}
+fn check(nbr: u64) -> bool {
+    for n in 2..nbr {
+        if nbr % n == 0 {
+        println!("{}, {}", n, nbr);
+            return false
+        }
+    }
+    true
+}
