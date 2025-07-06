e84963dd84cd7ae454ba5151e783b1f2de6d3b64
saljaoui <soufian2022ff@gmail.com>
2025-07-06 11:33:27 +0100

    update

--- last_exercises/matrix_multiplication/src/lib.rs
+++ last_exercises/matrix_multiplication/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#[derive(Debug, PartialEq)]
+pub struct Matrix (pub (i32, i32), pub (i32, i32));
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn multiply(m: Matrix, multiplier: i32) -> Matrix {
+    Matrix (
+        (m.0.0 * multiplier, m.0.1 * multiplier),
+        (m.1.0 * multiplier, m.1.1 * multiplier)
+    )
 }
