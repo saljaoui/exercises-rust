4ff470d7195f0757c64186d7e1c62d1cda3bf3ce
saljaoui <soufian2022ff@gmail.com>
2025-07-06 13:41:12 +0100

    update

--- last_exercises/previousprime/src/lib.rs
+++ last_exercises/previousprime/src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn prev_prime(nbr: u64) -> u64 {
+    
+    for i in (2..nbr).rev() {
+        if check(i) {
+            return i;
+        }
     }
+    0
+}
+fn check(n: u64) -> bool {
+    for i in 2..n {
+        if n % i == 0 && n != i {
+            return false;
+        }
+    }
+    true
 }
