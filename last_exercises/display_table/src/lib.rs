7953a7f84e04d3492c73ef36894176a4d0f190aa
saljaoui <soufian2022ff@gmail.com>
2025-07-06 21:29:05 +0100

    update

--- last_exercises/display_table/src/lib.rs
+++ last_exercises/display_table/src/lib.rs
@@ -1,14 +1,120 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Clone, Debug, PartialEq)]
+pub struct Table {
+	pub headers: Vec<String>,
+	pub body: Vec<Vec<String>>,
+}
+use std::fmt;
+
+impl fmt::Display for Table {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		// println!("{}", self);
+		if self.headers.len() == 0 {
+			return write!(f, "");
+		}
+	let mut vec: Vec<usize> = vec![];
+	for header in &self.headers {
+		// println!("{:?}", header.clone());
+		vec.push(header.len());
+	}
+
+	for b in &self.body {
+		for i in 0..b.len() {
+			if b[i].len() > vec[i] {
+				vec[i] = b[i].len()
+			}
+		}
+		
+	}
+
+	let mut res = String::new();
+
+	// for header in &self.headers {
+	// 	println!("{:?}", header.clone());
+	// 	let n_spaces = 
+	// 	// vec.push(header.len());
+	// }
+	res.push_str(&fix_words(&self.headers, &vec));
+	
+	res.push_str(&create_separator_line(&vec));
+	
+	for b in & self.body {
+		res.push_str(&fix_words(&b, &vec))
+	}
+	// println!("{:?}", res);
+
+		write!(f, "{}", res)
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn create_separator_line(vec_len: &Vec<usize>) -> String {
+	let mut res = String::new();
+	res.push('|');
+	for i in 0..vec_len.len() {
+		
+		for _ in 0..(vec_len[i] + 2) {
+			res.push('-');
+		}
+		if i < vec_len.len() - 1 {
+			res.push('+');
+		}
+		
+	}
+	res.push('|');
+	res.push('\n');
+	
+	res
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn for_space(n: usize) -> String {
+	let mut i = 0;
+	let mut res = String::new();
+	while i < n {
+		res.push(' ');
+		i +=1
+	}
+	res
+}
+
+fn fix_words(vec: &Vec<String>, vec_len: &Vec<usize>) -> String {
+	let mut res = String::new();
+	let mut add_space = false;
+	// println!("{:?}", vec);
+	for i in 0..vec.len() {
+		// println!("{:?}", vec[i]);
+		let mut n_spaces = vec_len[i] - vec[i].chars().count();
+		if n_spaces % 2 != 0 {
+			add_space = true;
+			n_spaces = n_spaces / 2;
+		} else {
+			n_spaces = n_spaces / 2;
+		}
+		res.push('|');
+		res.push_str(&for_space(n_spaces+1));
+		res.push_str(&vec[i]);
+		res.push_str(&for_space(n_spaces+1));
+
+
+		if add_space {
+			add_space = false;
+			res.push_str(&for_space(1));
+		}
+	}
+	res.push('|');
+	res.push('\n');
+
+	// println!("{:?}", res);
+	res
+}
+
+impl Table {
+	pub fn new() -> Table {
+        Table {
+            headers: Vec::new(),
+            body: Vec::new()
+        }
+	}
+
+	pub fn add_row(&mut self, row: &[String]) {
+		self.body.push(row.into())
+	}
 }
---
+++ last_exercises/display_table/src/main.rs
@@ -0,0 +1,125 @@
+use display_table::Table;
+
+fn main() {
+    let mut table = Table::new();
+    println!("{}", table);
+    table.headers = vec![
+        String::from("Model"),
+        String::from("Piece N°"),
+        String::from("In Stock"),
+        String::from("Description"),
+    ];
+    table.add_row(&[
+        String::from("model 1"),
+        String::from("43-EWQE304"),
+        String::from("30"),
+        String::from("Piece for x"),
+    ]);
+    table.add_row(&[
+        String::from("model 2"),
+        String::from("98-QCVX5433"),
+        String::from("100000000"),
+        String::from("-"),
+    ]);
+    table.add_row(&[
+        String::from("model y"),
+        String::from("78-NMNH"),
+        String::from("60"),
+        String::from("nothing"),
+    ]);
+    println!("{}", table);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use display_table::*;
+
+    #[test]
+    fn it_displays() {
+        let mut table = Table::new();
+        table.headers = vec![
+            "Name".to_string(),
+            "Last Name".to_string(),
+            "ID Number".to_string(),
+        ];
+        table.add_row(&[
+            "Ackerley".to_string(),
+            "Fillips".to_string(),
+            "123456789".to_string(),
+        ]);
+        table.add_row(&[
+            "Adamaris".to_string(),
+            "Fillips".to_string(),
+            "1111123456789".to_string(),
+        ]);
+        table.add_row(&[
+            "Ackerley".to_string(),
+            "Fillips".to_string(),
+            "123456789".to_string(),
+        ]);
+        assert_eq!(
+			table.to_string(),
+			"|   Name   | Last Name |   ID Number   |\n|----------+-----------+---------------|\n| Ackerley |  Fillips  |   123456789   |\n| Adamaris |  Fillips  | 1111123456789 |\n| Ackerley |  Fillips  |   123456789   |\n"
+		);
+    }
+
+    // An empty table must not display anything
+    #[test]
+    fn display_table_with_no_headers() {
+        let table = Table::new();
+        assert_eq!(table.to_string(), "");
+    }
+
+    #[test]
+    fn display_table_with_headers_only() {
+        let mut table = Table::new();
+        table.headers = vec![
+            "Name".to_string(),
+            "Last Name".to_string(),
+            "ID Number".to_string(),
+        ];
+        assert_eq!(
+            table.to_string(),
+            "| Name | Last Name | ID Number |\n|------+-----------+-----------|\n"
+        );
+    }
+
+    #[test]
+    fn display_second() {
+        let mut table = Table::new();
+        table.headers = vec![
+            "ID".to_string(),
+            "Car Brand".to_string(),
+            "Model".to_string(),
+            "Is Electric".to_string(),
+        ];
+        table.add_row(&[
+            "1".to_string(),
+            "Tesla".to_string(),
+            "Model 3".to_string(),
+            "True".to_string(),
+        ]);
+        table.add_row(&[
+            "2".to_string(),
+            "Ford".to_string(),
+            "Focus".to_string(),
+            "False".to_string(),
+        ]);
+        assert_eq!(
+			table.to_string(),
+			"| ID | Car Brand |  Model  | Is Electric |\n|----+-----------+---------+-------------|\n| 1  |   Tesla   | Model 3 |    True     |\n| 2  |   Ford    |  Focus  |    False    |\n"
+		);
+    }
+}
