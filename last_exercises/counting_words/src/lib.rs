fbb89570b99a31aacbf469d0a6cfaef322c90052
saljaoui <soufian2022ff@gmail.com>
2025-07-06 12:30:48 +0100

    update

--- last_exercises/counting_words/src/lib.rs
+++ last_exercises/counting_words/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+
+pub fn counting_words(words: &str) -> HashMap<String, u32> {
+    let mut hash_map: HashMap<String, u32> = HashMap::new();
+    let split: Vec<&str> = words.split_whitespace().collect();
+    for s in split {
+        let correct_word = correct_word(s);
+        if correct_word != "" {
+            *hash_map.entry(correct_word.to_string()).or_insert(0) += 1
+        }
+    }
+   
+    hash_map
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn correct_word(word: &str) -> String {
+    let mut res: String = String::new();
+    let chars: Vec<char> = word.chars().collect();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for (i, &w) in chars.iter().enumerate() {
+        if w.is_alphanumeric() {
+            res.push(w.to_ascii_lowercase());
+        } else if i > 0 && i < chars.len() - 1 && chars[i + 1].is_alphanumeric() && chars[i - 1].is_alphanumeric() {
+            res.push(w);
+        }
     }
+    res
 }
